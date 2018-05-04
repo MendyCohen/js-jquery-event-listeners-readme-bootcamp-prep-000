952337d655d34b884fce9dfb229c1e5bcf33fdd2
Mendy <mendybeltcohen@gmail.com>
2018-05-04 14:04:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,11 @@ $(document).ready(function(){
 });
 
 function getIt() {
+<<<<<<< HEAD
   $('p').click('eventname');
+=======
+  $('p').on('eventname');
+>>>>>>> 3043f4e21b4e3a838b12fa9111bc54403cb85a6e
   alert('Hey!');
 }
   
@@ -18,11 +22,18 @@ function frameIt() {
 }
 
 function pressIt() {
+<<<<<<< HEAD
   $('input').on('keydown', function(e){
     if(e.which == 71){
     alert();
     }
   });
+=======
+  $('form').on('keydown');
+  if($('input').val()=== 'G'){
+    return; 
+  }
+>>>>>>> 3043f4e21b4e3a838b12fa9111bc54403cb85a6e
 }
 
 function submitIt() {
